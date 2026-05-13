71db3750bc184ac03301187a53c513d1e194e0c5
Leonardo Pang <leonardopang12@gmail.com>
2026-05-13 17:22:51 -0300

    feat(case): update testimonials section and enhance case single layout

--- single-case.php
+++ single-case.php
@@ -3,6 +3,7 @@
 <div class="single-case s-border-sides">
   <?php $tpl_engine->partial('template/pages/single/cases/hero') ?>
   <?php $tpl_engine->partial('template/pages/single/cases/content') ?>
+  <?php $tpl_engine->partial('template/global/testimonials') ?>
   <?php $tpl_engine->partial('template/pages/single/cases/results') ?>
   <?php $tpl_engine->partial('template/pages/home/final-cta', ['data' => get_field('cta_case')]) ?>
 </div>
